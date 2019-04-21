25154e1fa370f0f6ffb36565f1ddb4e5a0515dae
Sajjad Hashemian <wolaws@gmail.com>
2019-04-21 23:08:57 +0430

    give credit

--- scripts/open_terminal.sh
+++ scripts/open_terminal.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# source https://www.reddit.com/r/swaywm/comments/bdseiq/modreturn_to_open_new_termite_window_in_the_same/el0h6js?utm_source=share&utm_medium=web2x
+
 terminal=${1:-alacritty}
 pid=$(swaymsg -t get_tree | jq '.. | select(.type?) | select(.type=="con") | select(.focused==true).pid')
 pname=$(ps -p ${pid} -o comm= | sed 's/-$//')
