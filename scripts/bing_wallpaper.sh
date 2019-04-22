276de0dbbdeb649684a2656a34c0e0404cb1a994
Sajjad Hashemian <wolaws@gmail.com>
2019-04-22 14:41:24 +0430

    fix wallpaper

--- scripts/bing_wallpaper.sh
+++ scripts/bing_wallpaper.sh
@@ -8,6 +8,6 @@ wluri=$(curl 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=e
 
 curl "https://www.bing.com$wluri" -s > $wlpath
 
-killall swaybg
+killall swaybg || true
 
 swaymsg "output * bg $wlpath fill"
