b8f1c0c92de20c54a75080d99d2b5e733e81bd35
Sajjad Hashemian <wolaws@gmail.com>
2019-04-22 12:44:38 +0430

    inhance bing wallpaper

---
+++ scripts/bing_wallpaper.sh
@@ -0,0 +1,13 @@
+#!/bin/env bash
+
+# exit on error
+set -e
+
+wlpath=${WALLPAPER_PATH:-"$HOME/wallpaper.jpg"}
+wluri=$(curl 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US' -s | jq '.images[0].url' --raw-output)
+
+curl "https://www.bing.com$wluri" -s > $wlpath
+
+killall swaybg
+
+swaymsg "output * bg $wlpath fill"
