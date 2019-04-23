dc2d09729fabd6ed889a2b0ab85168e48c4fdcea
Sajjad Hashemian <wolaws@gmail.com>
2019-04-23 08:39:04 +0430

    now it is possible to use it in crontab or systemd

--- scripts/bing_wallpaper.sh
+++ scripts/bing_wallpaper.sh
@@ -1,14 +1,22 @@
 #!/bin/env bash
 
+# systemd example: https://stackoverflow.com/a/50321912
+# cron example:
+# 0 * * * * ~/.config/sway/scripts/bing_wallpaper.sh
+
 # exit on error
 set -e
 
-baseurl="https://www.bing.com/"
+# for systemd or cron
+export SWAYSOCK=/run/user/$(id -u)/sway-ipc.$(id -u).$(pgrep -x sway).sock
+
 wlpath=${WALLPAPER_PATH:-"$HOME/wallpaper.jpg"}
+output=${WALLPAPER_OUTPUT:-"*"}
+baseurl="https://www.bing.com/"
 wluri=$(curl $baseurl"HPImageArchive.aspx?format=js&idx=0&n=20&mkt=en-US" -s | jq '.images[].url' --raw-output | shuf -n 1)
 
 curl "$baseurl$wluri" -s > $wlpath
 
 killall swaybg || true
 
-swaymsg "output * bg $wlpath fill"
+swaymsg "output $output bg $wlpath fill"
